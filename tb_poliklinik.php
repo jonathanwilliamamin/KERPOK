325687b2a5cea08c5382344175777123bc4daf0f
jonathanwilliamamin <paddi.techstuff@gmail.com>
2022-11-23 08:48:29 +0700

    tambah tb tb_poliklinik

---
+++ tb_poliklinik.php
@@ -0,0 +1,41 @@
+<?php
+require "functions.php";
+
+$poliklinik = query("SELECT * FROM tb_poliklinik");
+
+$i = 1;
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>TABEL POLIKLINIK</title>
+</head>
+<body>
+<div class="container">
+    <h1>Daftar Tabel Poliklinik</h1>
+    <a href="tambah.php">Tambah data</a>
+    <br></br>
+
+    
+
+    <table border="1" cellspacing="0" cellpadding="10">
+        <tr>
+            <th>ID Poli</th>
+            <th>Aksi</th>
+            <th>Nama Poli</th>
+            <th>Gedung</th>
+        </tr>
+        <tr>
+            <?php foreach ($poliklinik as $p) { ?>
+                <td> <?php echo $i; ?> </td>
+                <td> <a href="ubah.php?id_poli=<?= $p["id_poli"] ?>"> ubah </a> | <a href="hapus.php?id_poli=<?= $p["id_poli"] ?>"> hapus </a> </td>
+                <td> <?php echo $p["nama_poli"]; ?> </td>
+                <td> <?php echo $p["gedung"]; ?> </td>
+                <?php $i++; ?>  
+        </tr>
+            <?php } ?> 
+        </tr>
+    </table>
+</div>
+</body>
+</html>
