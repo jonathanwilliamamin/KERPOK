6c5f502e79737d4a1179e67b3e5004af0c343093
jonathanwilliamamin <paddi.techstuff@gmail.com>
2022-11-23 22:40:05 +0700

    pasien

---
+++ functions_tb_pasien.php
@@ -0,0 +1,18 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "relasitabeltest");
+
+function tambah($data) {
+    global $conn;
+
+    $nomor_identitas = $data["nomor_identitas"];
+    $nama_pasien = $data["nama_pasien"];
+    $jenis_kelamin = $data["jenis_kelamin"];
+    $alamat = $data["alamat"];
+    $no_telp = $data["no_telp"];
+
+    $query = "INSERT INTO tb_pasien VALUES ('', '$nomor_identitas', '$nama_pasien', '$jenis_kelamin', '$alamat', '$no_telp');";
+
+    mysqli_query($conn, $query);
+    return mysqli_affected_rows($conn);
+}
+?>
---
+++ tambah_tb_pasien.php
@@ -0,0 +1,52 @@
+<?php
+require 'functions_tb_pasien.php';
+
+if (isset($_POST["submit"])) {
+    if ( tambah($_POST) > 0 ) {
+        echo "<script>
+        alert('data berhasil ditambahkan!');
+        document.location.href = 'tb_pasien.php';
+        </script>";
+    } else {
+        echo "<script>
+        alert('data gagal ditambahkan!');
+        document.location.href = 'tb_pasien.php';
+        </script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah data</title>
+</head>
+<body>
+    <form action="" method="post"> 
+    <ul>
+        <li>
+            <label for="nomor_identitas">Nomor identitas: </label>
+            <input type="nomor_identitas" name="nomor_identitas" id="nomor_identitas">
+        </li>
+        <li>
+            <label for="nama_pasien">Nama Pasien: </label>
+            <input type="nama_pasien" name="nama_pasien" id="nama_pasien">
+        </li>
+        <li>
+            <label for="jenis_kelamin">Jenis Kelamin (L/P): </label>
+            <input type="jenis_kelamin" name="jenis_kelamin" id="jenis_kelamin">
+        </li>
+        <li>
+            <label for="alamat">Alamat: </label>
+            <input type="alamat" name="alamat" id="alamat">
+        </li>
+        <li>
+            <label for="no_telp">No. Telp: </label>
+            <input type="no_telp" name="no_telp" id="no_telp">
+        </li>
+        <button type="submit" name="submit">Submit</button>
+</body>
+</html>
